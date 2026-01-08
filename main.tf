a478ee3c40938fe493fb1b0fd7a38ef001c5b4c3
Winicius Silva <winicius.silva@bandtec.com.br>
2026-01-08 14:27:27 -0300

    feat: ajuste para permissoes no api-gateway

--- main.tf
+++ main.tf
@@ -31,6 +31,23 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_role_policy" "kms_decrypt_permission" {
+  name = "kms-decrypt-policy"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "AllowKMSDecrypt"
+        Effect   = "Allow"
+        Action   = ["kms:Decrypt"]
+        Resource = "arn:aws:kms:us-east-1:790144488941:key/d0db0b51-919b-4951-8e27-e278ca707bf7"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
@@ -96,5 +113,3 @@ resource "aws_lambda_permission" "api_gw" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
 }
-
-# Database / secrets variables moved to variables.tf
