dee880bc8642c11a1b2a1023aae715e20b2cd682
Filipe de Morais Pereira <fpereira@btcreditos.com.br>
2026-01-09 14:09:00 -0300

    feat: adicionando sufixo na chave de controle de estado

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tech-challenge-fiap-terraform-state"
-    key            = "tech-challenge-fiap-auth/terraform.tfstate"
+    key            = "tech-challenge-fiap-auth/terraform-${var.environment}.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tech-challenge-fiap-terraform-locks"
     encrypt        = true
